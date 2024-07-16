c66d5beceb6f51cf5d9dfbe3113f0c67c71a28fc
sharmilasha <sharmilaajii@gmail.com>
2024-07-16 13:11:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+
+import Stopwatch from './components/stopwatch';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Stopwatch />
+    </div>
+  );
+};
+
+export default App;
